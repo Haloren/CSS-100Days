81a26ed5c848fedbb4541217520662b37cf25e96
Haloren <mckennakyler@gmail.com>
2021-01-24 01:13:28 -0700

    added reset on 'mouseleave' to 049

--- 049/index.js
+++ 049/index.js
@@ -1,9 +1,16 @@
 const frame = document.getElementById('frame');
 const circle = document.getElementById('circle');
 
-document.addEventListener('mousemove', (e) => {
+frame.addEventListener('mousemove', (e) => {
     e.preventDefault();
     circle.style.transition = 'all .2s';
     circle.style.left = e.offsetX + 'px';
     circle.style.top = e.offsetY + 'px';
 });
+
+frame.addEventListener('mouseleave', (e) => {
+    e.preventDefault();
+    circle.style.transition = 'all .4s';
+    circle.style.left = '50%';
+    circle.style.top = '50%';
+})
