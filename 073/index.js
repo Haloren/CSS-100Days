99ec752884a011a82afe937796e819dc00fd089b
Haloren <mckennakyler@gmail.com>
2021-02-19 19:33:19 -0700

    changed saturns pic

--- 073/index.js
+++ 073/index.js
@@ -1,2 +1,28 @@
-wordsArray = [Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto]
-pictureArray = []
+const center = document.querySelector('.center');
+
+const planets = [
+    ['Mercury', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/1Mercury.jpg'],
+    ['Venus', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/2Venus.jpg'],
+    ['Earth', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/3Earth.jpg'],
+    ['Mars', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/4Mars.jpg'],
+    ['Jupiter', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/5Jupiter.jpg'],
+    ['Saturn', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/6Saturn.png'],
+    ['Uranus', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/7Uranus.jpg'],
+    ['Neptune', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/8Neptune.jpg'],
+    ['Pluto', 'https://raw.githubusercontent.com/Haloren/CSS-100Days/main/073/images/Pluto.jpg']
+]
+
+renderPlanetCard = () => {
+    let h1 = document.createElement('h1');
+    h1.setAttribute('id', 'planetName')
+    h1.innerHTML = planets[3][0]
+
+    let img = document.createElement('img');
+    img.setAttribute('id', 'planetImage')
+    img.src = planets[3][1]
+
+    center.appendChild(h1)
+    center.appendChild(img)
+}
+
+renderPlanetCard();
