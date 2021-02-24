93afc951fbf3fb2f8ee4add7b412112122e631d0
Haloren <mckennakyler@gmail.com>
2021-02-23 22:13:42 -0700

    changed css not:focus:valid to js functions

--- 076/index.js
+++ 076/index.js
@@ -1,6 +1,9 @@
-const inputPassword = document.getElementById('input-password')
-const inputLabel = 
+const inputPassword = document.getElementById('input-password');
+const inputLabel = document.getElementById('password-label');
 
 showPassword = () => {
     inputPassword.type === 'password' ? inputPassword.type = 'text' : inputPassword.type = 'password'
 }
+
+showLabel = () => { inputLabel.style.opacity = 1 }
+blurLabel = () => { inputLabel.style.opacity = 0 }
