5100950d0857f0443d1e581612556021271d7c05
Haloren <mckennakyler@gmail.com>
2021-02-23 21:56:52 -0700

    password label shows onFocus

--- 076/index.js
+++ 076/index.js
@@ -1,4 +1,5 @@
 const inputPassword = document.getElementById('input-password')
+const inputLabel = 
 
 showPassword = () => {
     inputPassword.type === 'password' ? inputPassword.type = 'text' : inputPassword.type = 'password'
