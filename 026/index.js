9b64fe02f9843909518a0e384dc442b2d427a1aa
Haloren <mckennakyler@gmail.com>
2021-04-22 22:03:55 -0700

    026 continued need to add js

--- 026/index.js
+++ 026/index.js
@@ -3,4 +3,11 @@ quote = [   'If you want to grow, get outside your comfort zone.',
             'Pick yourself up, dust yourself off, and start again.', 
             'Always be yourself, unless you can be a unicorn.'
         ]
-card = document.getElementById('card')
+card = document.getElementById('card');
+cardNumber = document.getElementById('pg-number');
+cardTitle = document.getElementById('title');
+cardQuote = document.getElementById('quote');
+
+const addContent = (index) => {
+    cardNumber.value = index;
+}
