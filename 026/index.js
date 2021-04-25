53742054108ec7230ad59480e09eee4ca34ae012
Haloren <mckennakyler@gmail.com>
2021-04-25 16:25:55 -0700

    card info changes on click

--- 026/index.js
+++ 026/index.js
@@ -3,11 +3,23 @@ quote = [   'If you want to grow, get outside your comfort zone.',
             'Pick yourself up, dust yourself off, and start again.', 
             'Always be yourself, unless you can be a unicorn.'
         ]
-card = document.getElementById('card');
-cardNumber = document.getElementById('pg-number');
+
+center = document.getElementById('center');
+cardNumber = document.getElementById('card-number');
 cardTitle = document.getElementById('title');
 cardQuote = document.getElementById('quote');
 
-const addContent = (index) => {
-    cardNumber.value = index;
+let count = 0;
+getIndex = () => {
+    count < title.length - 1 ? count++ : count=0;
+    return count
 }
+
+const createQuote = (num) => {
+    let index = num;
+    cardNumber.innerHTML = index + 1;
+    cardTitle.innerHTML = title[index];
+    cardQuote.innerHTML = quote[index];
+}
+
+createQuote(0);
