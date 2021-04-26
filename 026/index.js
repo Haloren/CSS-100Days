78b58c2971a465df7f9274dd5b563a8cbb6d2a4d
Haloren <mckennakyler@gmail.com>
2021-04-25 17:13:24 -0700

    finsihed 026

--- 026/index.js
+++ 026/index.js
@@ -1,10 +1,11 @@
 title = ['The couch', 'Failing is learning', 'Flowers and rainbows']
 quote = [   'If you want to grow, get outside your comfort zone.', 
             'Pick yourself up, dust yourself off, and start again.', 
-            'Always be yourself, unless you can be a unicorn.'
+            'Always be yourself, <br> unless you can be a unicorn.'
         ]
 
 center = document.getElementById('center');
+card = document.getElementById('card');
 cardNumber = document.getElementById('card-number');
 cardTitle = document.getElementById('title');
 cardQuote = document.getElementById('quote');
@@ -20,6 +21,11 @@ const createQuote = (num) => {
     cardNumber.innerHTML = index + 1;
     cardTitle.innerHTML = title[index];
     cardQuote.innerHTML = quote[index];
+    
+    //Re-runs css card loading animation
+    card.classList.remove('switch');
+    void card.offsetWidth;
+    card.classList.add('switch');
 }
 
 createQuote(0);
